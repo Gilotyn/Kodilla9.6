e454a97a44be0ae618de0606d8907cbba1f3a309
Robert Graupner <robertgraupner@air-robert.home>
2017-07-02 11:09:48 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var list = document.getElementById('list');
+var	add = document.getElementById('addElem');
+
+	add.addEventListener('click', function() {
+		var element = document.createElement('li');
+		var number = document.getElementsByTagName('li');
+		element.innerHTML = 'item ' + number.length;
+  		list.appendChild(element);
+});
